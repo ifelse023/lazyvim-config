e0ed0be311dd503a2ec7a51dcbebcae749a149b4
ifelse023 <ifelse023@gmail.com>
2025-02-19 19:37:52 +0100

    :)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,7 +3,7 @@
 -- Add any additional keymaps here
 --
 
-vim.keymap.set("n", "<S-CR>", "<cmd>wincmd w<cr>") -- Switch to the next window
+vim.keymap.set("n", "<A-CR>", "<cmd>wincmd w<cr>") -- Switch to the next window
 vim.keymap.set("n", "<Tab>", "<cmd>bnext<cr>")
 
 vim.keymap.set("n", "<leader>cx", function()
@@ -14,17 +14,8 @@ vim.keymap.set("n", "<leader>cz", function()
   require("cmp").setup.buffer({ enabled = true })
 end, { desc = "Enable buffer completion" })
 
--- Recenter the screen on the current line using 'zz'
-vim.keymap.set("n", "<leader>z", "zz", { desc = "Recenter screen" })
+vim.keymap.set("n", "<leader>zz", "zz", { desc = "Recenter screen" })
 
-if vim.g.neovide then
-  vim.keymap.set("n", "<C-s>", ":w<CR>") -- Save
-  vim.keymap.set("v", "<C-c>", '"+y') -- Copy
-  vim.keymap.set("n", "<C-v>", '"+P') -- Paste normal mode
-  vim.keymap.set("v", "<C-v>", '"+P') -- Paste visual mode
-  vim.keymap.set("c", "<C-v>", "<C-R>+") -- Paste command mode
-  vim.keymap.set("i", "<C-v>", '<ESC>l"+Pli') -- Paste insert mode
-end
 vim.g.neovide_scale_factor = 1.0
 local change_scale_factor = function(delta)
   vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
@@ -35,8 +26,17 @@ end)
 vim.keymap.set("n", "<C-->", function()
   change_scale_factor(1 / 1.25)
 end)
+if vim.g.neovide then
+  vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+end
+
 -- Allow clipboard copy paste in neovim
-vim.api.nvim_set_keymap("", "<C-v>", "+p<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("!", "<C-v>", "<C-R>+", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("t", "<C-v>", "<C-R>+", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<C-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
