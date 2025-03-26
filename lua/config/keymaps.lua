51ca04370628709c1993be6319526af6f22a547e
ifelse023 <ifelse023@gmail.com>
2025-03-26 03:12:25 +0100

    :)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,25 +1,22 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
 vim.keymap.set("n", "<leader>wn", ':vsplit | lua Snacks.dashboard.pick("files")<CR>', {
   noremap = true,
   silent = true,
   desc = "Split window and open file picker",
 })
-----------------------------------------------
--- Window and Tab Navigation
-----------------------------------------------
--- Switch to the next window
-vim.keymap.set("n", "<Tab>", "<cmd>wincmd w<cr>", { desc = "Switch to next window" }) -- Duplicated mapping
--- Ctrl-Tab to jump to previous window
-vim.keymap.set("n", "<C-Tab>", "<cmd>wincmd W<cr>", { desc = "Switch to previous window" })
--- Split window vertically (to the right)
+
+vim.keymap.set("n", "<A-h>", "<cmd>wincmd h<cr>", { desc = "Move to left window" })
+vim.keymap.set("n", "<A-j>", "<cmd>wincmd j<cr>", { desc = "Move to below window" })
+vim.keymap.set("n", "<A-k>", "<cmd>wincmd k<cr>", { desc = "Move to above window" })
+vim.keymap.set("n", "<A-l>", "<cmd>wincmd l<cr>", { desc = "Move to right window" })
 vim.keymap.set("n", "<A-s>", "<cmd>vsplit<CR>", { desc = "Split window vertically" })
+vim.keymap.set("n", "<A-v>", "<cmd>split<CR>", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<A-q>", "<cmd>close<CR>", { desc = "Close current window" })
+vim.keymap.set("n", "<Tab>", "<cmd>wincmd w<cr>", { desc = "Switch to next window" }) -- Duplicated mapping
+vim.keymap.set("n", "<C-Tab>", "<cmd>wincmd W<cr>", { desc = "Switch to previous window" })
 
 -- Center the cursor in the window
 vim.keymap.set("n", "zz", "zz", { desc = "Recenter screen" })
 
--- Using Alt+w for next window (works in terminal mode too)
 vim.keymap.set({ "n", "t" }, "<A-w>", function()
   if vim.fn.mode() == "t" then
     -- If in terminal mode, first switch to normal mode in the terminal
@@ -31,58 +28,12 @@ vim.keymap.set({ "n", "t" }, "<A-w>", function()
     vim.cmd("wincmd w")
   end
 end, { desc = "Switch to next window (terminal-aware)" })
-----------------------------------------------
--- Buffer Navigation
-----------------------------------------------
--- Go to the next buffer
+
 vim.keymap.set("n", "<A-Tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
-
--- Create a new tab
 vim.keymap.set("n", "<A-t>", "<cmd>tabnew<CR>", { desc = "New tab" })
+vim.keymap.set("n", "<A-d>", "<cmd>bd<CR>", { desc = "Delete buffer" })
 
-vim.keymap.set("n", "<A-q>", "<cmd>bd<CR>", { desc = "Delete buffer" })
-
-----------------------------------------------
--- Line Navigation
-----------------------------------------------
--- Jump down 10 lines and center cursor
-vim.keymap.set("n", "<leader>j", "10jzz", { desc = "Jump down 10 lines and center" })
--- Jump up 10 lines and center cursor
-vim.keymap.set("n", "<leader>k", "10kzz", { desc = "Jump up 10 lines and center" })
-
--- Alternative mappings using Alt key
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up and center" })
 vim.keymap.set("n", "<C-j>", "10jzz", { desc = "Jump down 10 lines and center" })
 vim.keymap.set("n", "<C-k>", "10kzz", { desc = "Jump up 10 lines and center" })
-----------------------------------------------
--- Neovide Specific Settings
-----------------------------------------------
-if vim.g.neovide then
-  vim.keymap.set("n", "<C-s>", ":w<CR>") -- Save
-  vim.keymap.set("v", "<C-c>", '"+y') -- Copy
-  vim.keymap.set("n", "<C-v>", '"+P') -- Paste normal mode
-  vim.keymap.set("v", "<C-v>", '"+P') -- Paste visual mode
-  vim.keymap.set("c", "<C-v>", "<C-R>+") -- Paste command mode
-  vim.keymap.set("i", "<C-v>", '<ESC>l"+Pli') -- Paste insert mode
-end
-
--- Allow clipboard copy paste in neovim
-vim.api.nvim_set_keymap("", "<C-v>", "+p<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("!", "<C-v>", "<C-R>+", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("t", "<C-v>", "<C-R>+", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<C-v>", "<C-R>+", { noremap = true, silent = true })
--- Zoom in and out with Ctrl+= and Ctrl+-
-
-vim.g.neovide_scale_factor = 1.0
-local change_scale_factor = function(delta)
-  vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
-end
-vim.keymap.set("n", "<C-=>", function()
-  change_scale_factor(1.25)
-end)
-vim.keymap.set("n", "<C-->", function()
-  change_scale_factor(1 / 1.25)
-end)
-
-vim.keymap.set("n", "<C-0>", function()
-  vim.g.neovide_scale_factor = 1.0
-end, { desc = "Reset font size" })
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,28 +1,9 @@
 return {
 
   {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
+    "LazyVim/LazyVim",
     opts = {
-      variant = "dark",
-      overrides = function(colors)
-        return {
-          FlashBackdrop = { fg = colors.bg_highlight },
-          FlashLabel = { fg = colors.blue, bg = colors.bg, bold = true },
-          FlashMatch = { fg = colors.magenta, bg = colors.bg },
-          FlashCurrent = { fg = colors.green, bg = colors.bg },
-          FlashPrompt = { link = "NormalFloat" },
-          ["@keyword.return"] = { fg = "#5ef1ff", bold = true },
-          ["@character.printf"] = { fg = "#2ac3de" },
-        }
-      end,
-    },
-    {
-      "LazyVim/LazyVim",
-      opts = {
-        colorscheme = "catppuccin",
-      },
+      colorscheme = "tokyonight-night",
     },
   },
 }
