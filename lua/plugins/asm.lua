8d1a3a3e2bee33f022dc38f5aaaab94d4ab38cca
ifelse023 <ifelse023@gmail.com>
2025-07-22 02:38:08 +0200

    :)

---
+++ lua/plugins/asm.lua
@@ -0,0 +1,14 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = function(_)
+    local lspconfig = require("lspconfig")
+    lspconfig.asm_lsp.setup({
+      cmd = { "asm-lsp" },
+      filetypes = { "asm", "S" },
+      root_dir = lspconfig.util.root_pattern(".git", "justfile"),
+      settings = {
+        asm_lsp = {},
+      },
+    })
+  end,
+}
